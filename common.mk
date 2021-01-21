3ba942d5adf28e0a34fe1a3e3a0e3e90c474fff6
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-01-21 10:47:44 +0000

    sm7125-common: add com.google.android.modulemetadata prebuilt apk 	-helps to update Gplay security

--- common.mk
+++ common.mk
@@ -213,6 +213,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Prebuilt Apk
+PRODUCT_PACKAGES += \
+    com.google.android.modulemetadata
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
