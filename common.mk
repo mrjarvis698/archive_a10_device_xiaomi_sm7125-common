30a5b937baa9fb1ec73cf587681f98c99faea0e3
Mr.Jarvis698 <dudamajaykumar59@gmail.com>
2021-01-26 00:28:42 -0800

    sm7125-common: forcefully inherit common vendor

--- common.mk
+++ common.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit proprietary targets
-$(call inherit-product-if-exists, vendor/xiaomi/sm7125-common/sm7125-common-vendor.mk)
+$(call inherit-product, vendor/xiaomi/sm7125-common/sm7125-common-vendor.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
